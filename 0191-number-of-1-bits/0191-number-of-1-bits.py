184a049029e75aede03d5c7c434bd6073a6e29f8
Batool Kazmi <112828911+BatoolKazmi@users.noreply.github.com>
2026-06-06 06:36:31 -0400

    Time: 0 ms (100%), Space: 19.1 MB (86.5%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -3,9 +3,9 @@ class Solution:
         res = 0 
 
         while n:
-            res += n % 2
-            n = n >> 1
-
+            n &= (n - 1)
+            res += 1
+            
         return res 
 
         
