8f0fe37b4d663ec117cce10c1e55cffa81eea9d6
Batool Kazmi <112828911+BatoolKazmi@users.noreply.github.com>
2026-06-28 08:32:02 -0400

    Time: 0 ms (100%), Space: 19.9 MB (80.41%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,6 +1,6 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+        l, r = 0, len(nums)
 
         while l < r: 
             mid = (l + r) // 2
@@ -10,4 +10,4 @@ class Solution:
             else:
                 r = mid
             
-        return l if nums[l] >= target else l + 1
+        return l 
